1f5edf2483983b1c7675129bfc2ab5697a844919
Venus <bisnesbrend@gmail.com>
2022-09-15 22:50:07 +0300

    Добавьте файлы проекта.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MegaDuctuionary<int, string> myDick = new MegaDuctuionary<int, string>(
+                    new KeyValuePair<int, string>(11, "Привет"),
+                    new KeyValuePair<int, string>(12, "Пока"));
+
+
+
+            foreach (var item in myDick)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class MegaDuctuionary<Key, Value>
+    {
+        KeyValuePair<Key, Value>[] massiv;
+
+        public MegaDuctuionary(params KeyValuePair<Key, Value>[] znachenia)
+        {
+            massiv = znachenia;
+
+        }
+
+        public MegaDuctuionary<Key, Value> GetEnumerator()
+        {
+            return this;
+        }
+
+        public KeyValuePair<Key, Value> Current
+        {
+            get 
+            { 
+                return massiv[position]; 
+            }
+        }
+
+
+        int position = -1;
+        public bool MoveNext()
+        {
+            if (position < massiv.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            position = -1;
+            return false;
+        }
+    }
+}
---
+++ YourCollection/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<Stolbec> list = new MyList<Stolbec>();
+
+            foreach (var item in list)
+            {
+                item.Id = 123;
+            }
+        }
+    }
+
+    class Stolbec
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class MyList<Type> 
+    {
+
+        Type[] massiv = new Type[0];
+
+        public MyList()
+        {
+                
+        }
+
+        public MyList<Type> GetEnumerator()
+        {
+            return this;
+        }
+
+        int position = -1;
+        public bool MoveNext()
+        {
+            if( position < massiv.Length)
+            {
+                position++;
+                return true;
+            }
+            position = -1;
+            return false;
+        }
+
+        public Type Current
+        {
+            get
+            {
+                return massiv[position];
+            }
+        }
+    }
+}
